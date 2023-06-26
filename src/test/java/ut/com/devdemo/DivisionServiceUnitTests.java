625e6604b5f8c545cdfd872590d718d41f358546
Decree Labs <88178632+decreelabs@users.noreply.github.com>
2023-06-26 11:52:57 +1000

    Initial plugin skeleton
    Initial plugin skeleton

---
+++ src/test/java/ut/com/devdemo/DivisionServiceUnitTests.java
@@ -0,0 +1,31 @@
+package ut.com.devdemo;
+
+import org.junit.Test;
+import com.devdemo.api.MyPluginComponent;
+import com.devdemo.impl.MyPluginComponentImpl;
+
+import static org.junit.Assert.assertEquals;
+
+public class DivisionServiceUnitTests
+{
+    @Test
+    public void testDivisionService()
+    {
+        Integer sum = 10;
+        assertEquals(10, sum.intValue());
+    }
+
+    @Test
+    public void testDivisionService2()
+    {
+        Integer sum = 10;
+        assertEquals(10, sum.intValue());
+    }
+
+    @Test
+    public void testDivisionService3()
+    {
+        Integer sum = 10;
+        assertEquals(10, sum.intValue());
+    }
+}
